0ea288ddad6768499aecc9ced2bb91fa578afb32
Kieran Crown <kieran.crown@vpress.com>
2021-10-31 14:52:32 +0000

    Changed IP

--- frontend/components/lights.tsx
+++ frontend/components/lights.tsx
@@ -6,7 +6,7 @@ export default function LightsView() {
     const [lights, setLights] = useState<Lights>();
 
     const getLights = async () => {
-        const res = await fetch('http://127.0.0.1:8080/hue/lights', { cache: 'no-cache' });
+        const res = await fetch('http://192.168.10.95:8080/hue/lights', { cache: 'no-cache' });
         if (res.status === 200) {
             setLights((await res.json()) as Lights);
         } else {
@@ -19,7 +19,7 @@ export default function LightsView() {
     }, []);
 
     const toggleOn = async (id: string, state: LightState) => {
-        const res = await fetch('http://127.0.0.1:8080/hue/lights/state', {
+        const res = await fetch('http://192.168.10.95:8080/hue/lights/state', {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
