0158a6054a4d30177cc547b2de00d2ee2bc97736
Kieran Crown <kieran.crown@vpress.com>
2021-11-03 16:52:52 +0000

    Added manifest for frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,7 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+/* eslint-disable */
+const withOffline = require('next-offline');
+// your next.js configs
+const nextConfig = {
     webpackDevMiddleware: (config) => {
         config.watchOptions = {
             poll: 1000,
@@ -9,3 +11,5 @@ module.exports = {
     },
     reactStrictMode: true,
 };
+
+module.exports = withOffline(nextConfig);
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,20 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return (
-        <div className="p-6">
-            <Component {...pageProps} />
-        </div>
+        <>
+            <Head>
+                <link rel="manifest" href="/manifest.json" />
+                <meta name="theme-color" content="#90cdf4" />
+                <link rel="apple-touch-icon" href="/logo-96x96.png" />
+                <meta name="apple-mobile-web-app-status-bar" content="#90cdf4" />
+            </Head>
+            <div className="p-6">
+                <Component {...pageProps} />
+            </div>
+        </>
     );
 }
 
