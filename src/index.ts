2fb51c22a2038afdba9d23ec537c2fe4b8086e8c
Kieran Crown <kieran.crown@vpress.com>
2021-11-03 15:40:55 +0000

    Added monogo db test

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import * as dotenv from 'dotenv';
 import { initControllers } from './controllers';
+import { MongoDB } from './helpers/mongo';
 import { WebServer } from './server';
 
 async function main() {
     dotenv.config();
+    const database = MongoDB.getInstance();
+    // TEST
+    if (process.env.MONGODB_URL && process.env.DB_NAME) {
+        await database.connect(process.env.MONGODB_URL, process.env.DB_NAME);
+    }
     const server = WebServer.getInstance();
     server.startServer(Number(process.env.SERVER_PORT) || 3000);
     await initControllers();
